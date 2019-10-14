fce8274b1351d3207f5521f40dac96a846d745b8
BhushanRaghuwanshi17 <54661018+BhushanRaghuwanshi17@users.noreply.github.com>
2019-10-14 11:00:59 +0530

    Add files via upload

---
+++ MVC-Internationalization/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Threading;
+using System.Globalization;
+using MVC_Internationalization.Models;
+namespace MVC_Internationalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        //initilizing culture on controller initialization
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if (Session["CurrentCulture"] != null)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(Session["CurrentCulture"].ToString());
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Session["CurrentCulture"].ToString());
+            }
+        }
+
+        // changing culture
+        public ActionResult ChangeCulture(string ddlCulture)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(ddlCulture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(ddlCulture);
+
+            Session["CurrentCulture"] = ddlCulture;
+            return View("Index");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                //TO DO:
+            }
+            return View();
+        }
+    }
+}
